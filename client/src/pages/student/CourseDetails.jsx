42d0ea3acd64daf281eca4225f9e9ec04f29dfa6
Abdul Hannan <abdulhannanhere@gmail.com>
2025-07-17 01:50:56 +0500

    Course Details completed

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -12,10 +12,15 @@ import {
 import { Card } from "@/components/ui/card";
 import { assets } from "@/assets/assets";
 import humanizeDuration from "humanize-duration";
+import YouTube from "react-youtube";
 
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
+  const [playerData, setPlayerData] = useState(null);
+  console.log(playerData);
 
   const {
     allCourses,
@@ -32,7 +37,7 @@ const CourseDetails = () => {
   useEffect(() => {
     fetchCourseData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [allCourses]);
 
   return courseData ? (
     <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-20 pt-10 text-left">
@@ -110,14 +115,23 @@ const CourseDetails = () => {
                               <img
                                 src={assets.play_icon}
                                 alt="play icon"
-                                className="w-4 h-4 mt-1"
+                                className="w-4 h-4 mt-1 cursor-pointer"
                               />{" "}
                               {lecture.lectureTitle}
                             </span>
                             <span className="text-sm flex gap-2">
                               {/* Preview 12 hours */}
                               {lecture.isPreviewFree && (
-                                <span className="text-sm text-blue-600 cursor-pointer">
+                                <span
+                                  onClick={() =>
+                                    setPlayerData({
+                                      videoId: lecture.lectureUrl
+                                        .split("/")
+                                        .pop(),
+                                    })
+                                  }
+                                  className="text-sm text-blue-600 cursor-pointer"
+                                >
                                   Preview
                                 </span>
                               )}
@@ -151,8 +165,21 @@ const CourseDetails = () => {
       </div>
 
       {/* Right Column */}
-      <div className="max-w-[424px] z-10 shadow-xl rounded-t-lg overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
-        <img src={courseData.courseThumbnail} alt="course thumbnail" />
+      <div className="max-w-[424px] z-10 shadow-xl rounded-lg overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
+        {playerData ? (
+          <YouTube
+            videoId={playerData.videoId}
+            opts={{
+              playerVars: {
+                autoplay: 1,
+              },
+            }}
+            iframeClassName="w-full aspect-video"
+          />
+        ) : (
+          <img src={courseData.courseThumbnail} alt="course thumbnail" />
+        )}
+
         <div className="p-5">
           <div className="flex items-center gap-2">
             <img
@@ -198,6 +225,22 @@ const CourseDetails = () => {
               <img src={assets.lesson_icon} alt="lesson icon" />
               <p>{calculateNoOfLectures(courseData)} lessons</p>
             </div>
+          </div>
+          <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
+            {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+          </button>
+
+          <div className="pt-6">
+            <p className="md:text-xl text-lg font-medium text-gray-800">
+              What's in the course?
+            </p>
+            <ul className="ml-4 pt-2 text-sm md:text-default list-disc text-gray-500">
+              <li>Lifetime access with free updates.</li>
+              <li>Step-by-step, hands-on project guidance.</li>
+              <li>Downloadable resources and source code.</li>
+              <li>Quizzes to test your knowledge.</li>
+              <li>Certificate of completion.</li>
+            </ul>
           </div>
         </div>
       </div>
